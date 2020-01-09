b1288164b2e5814d21036c4cb3873e3f610c16ca
12tqian <43422753+12tqian@users.noreply.github.com>
2020-01-09 18:23:46 -0500

    a

---
+++ src/tqtestbot/Action.java
@@ -0,0 +1,11 @@
+package tqtestbot;
+import battlecode.common.*;
+import java.util.*;
+
+public class Action {
+    int type = -1;
+    Direction dir = Direction.CENTER;
+    RobotType unit = RobotType.COW;
+    int soup = -1;
+    int id = -1;
+}
---
+++ src/tqtestbot/Movable.java
@@ -0,0 +1,11 @@
+package tqtestbot;
+import battlecode.common.*;
+import java.util.*;
+
+public class Movable {
+    public RobotPlayer Z;
+    public Movable(RobotPlayer z){
+        Z = z;
+    }
+
+}
