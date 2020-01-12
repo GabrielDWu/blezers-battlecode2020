55ce1cb618d9740573a140e6ae43f904fb6a4144
12tqian <43422753+12tqian@users.noreply.github.com>
2020-01-12 00:47:57 -0500

    a

--- src/blezerbot/units/DeliveryDrone.java
+++ src/blezerbot/units/DeliveryDrone.java
@@ -5,9 +5,20 @@ import java.util.*;
 import blezerbot.*;
 
 public class DeliveryDrone extends Unit {
-
+	enum DeliveryDroneStatus {
+		PICK_UP,
+		DROP_OFF,
+		SEARCHING,
+		FIND_ENEMY_HQ,
+		RETURNING,
+		NOTHING
+	}
+	DeliveryDroneStatus status;
 	boolean findingEnemyHQ;
 	MapLocation[] enemyHQs;
+	MapLocation lastSeen;
+	MapLocation dropLocation;
+	int searchID = 0;
 	int enemyHQc;
 
 	public DeliveryDrone(RobotController rc) throws GameActionException {
@@ -17,16 +28,74 @@ public class DeliveryDrone extends Unit {
 	public void run() throws GameActionException {
 		super.run();
 		if (enemyHQs == null) {
-			findingEnemyHQ = true;
+			status = DeliveryDroneStatus.FIND_ENEMY_HQ;
 			enemyHQs = new MapLocation[3];
 			enemyHQc = -1;
 		}
+		if(rc.isCurrentlyHoldingUnit() && status != DeliveryDroneStatus.DROP_OFF){
+			if(rc.canDropUnit(Direction.CENTER)){
+				rc.dropUnit(Direction.CENTER);
+			}
+		}
 		if (locHQ == null) return;
-		if (findingEnemyHQ) {
+		if (status == DeliveryDroneStatus.FIND_ENEMY_HQ) {
 			MapLocation enemyHQ = findEnemyHQ();
 			if (enemyHQ != null) System.out.println("FOUND "+enemyHQ);
 		}
+		else if (status == DeliveryDroneStatus.PICK_UP && rc.isCurrentlyHoldingUnit() == false){
+			pickUpID();
+		}
+		else if (status == DeliveryDroneStatus.DROP_OFF){
+			dropOff();
+		}
+		else if (status == DeliveryDroneStatus.RETURNING){
+			goTo(locHQ);
+		}
 	}
+	void dropOff() throws GameActionException {
+		if(rc.canSenseLocation(dropLocation)) {
+			if (rc.isLocationOccupied(dropLocation) ) {
+				if (rc.getLocation().isAdjacentTo(dropLocation)) {
+					rc.dropUnit(rc.getLocation().directionTo(dropLocation));
+					status = DeliveryDroneStatus.NOTHING;
+					return;
+				}
+				goTo(dropLocation);
+			}
+			else{
+				if(rc.canDropUnit(Direction.CENTER)){
+					rc.dropUnit(Direction.CENTER);
+				}
+				status = DeliveryDroneStatus.RETURNING;
+				return;
+			}
+		}
+		goTo(dropLocation);
+
+	}
+	void pickUpID() throws GameActionException {
+		RobotInfo[] robots = rc.senseNearbyRobots();
+		for(RobotInfo r: robots){
+			if(r.getID() == searchID){
+				lastSeen = r.getLocation();
+				chasePickUp(r);
+				return;
+			}
+		}
+		goTo(lastSeen);
+
+	}
+
+	void chasePickUp(RobotInfo target) throws GameActionException {
+		if(rc.getLocation().isAdjacentTo(target.getLocation())){
+			rc.pickUpUnit(searchID);
+			status = DeliveryDroneStatus.DROP_OFF;
+			return;
+		}
+		/// for now
+		goTo(target.getLocation());
+	}
+
 
 	public MapLocation findEnemyHQ() throws GameActionException {
 		if (enemyHQc == -1) {
@@ -81,7 +150,7 @@ public class DeliveryDrone extends Unit {
 	}
 
 	public boolean canMove(Direction dir) throws GameActionException {
-		return rc.canMove(dir);
+		return canMove(dir)&&(!netGunRadius(rc.getLocation().add(dir)));
 	}
 
 }
