425d95792907e7f0a6264b4a165e9c2e65cda93b
GabrielDWu <43422691+GabrielDWu@users.noreply.github.com>
2020-01-20 14:36:36 -0500

    design school + attacks

--- src/blezerbot/buildings/HQ.java
+++ src/blezerbot/buildings/HQ.java
@@ -87,13 +87,21 @@ public class HQ extends Building {
 			hq_sentLoc = true;
 		}
 
-		if (units[2 /*refinery*/].size() == 0 && units[1].size() > 0 /*miner*/ && waitingForBuilding > 10 && rc.getTeamSoup() > 200) {
-			waitingForBuilding = 1;
-			ArrayList<InternalUnit> miners = units[RobotType.MINER.ordinal()];
-			InternalUnit miner = miners.get(r.nextInt(miners.size()));
-			writeMessage(Message.build(RobotType.REFINERY, miner.id));
-			addMessageToQueue();
-		}
+        if (units[2 /*refinery*/].size() == 0 && units[1].size() > 0 /*miner*/ && waitingForBuilding > 10 && rc.getTeamSoup() > 200) {
+            waitingForBuilding = 1;
+            ArrayList<InternalUnit> miners = units[RobotType.MINER.ordinal()];
+            InternalUnit miner = miners.get(r.nextInt(miners.size()));
+            writeMessage(Message.build(RobotType.REFINERY, miner.id));
+            addMessageToQueue();
+        }
+        if (landscaperWalled && units[5 /*fc*/].size() == 0 && units[1].size() > 0 /*miner*/ && waitingForBuilding > 10 && rc.getTeamSoup() > 200) {
+            waitingForBuilding = 1;
+            ArrayList<InternalUnit> miners = units[RobotType.MINER.ordinal()];
+            InternalUnit miner = miners.get(r.nextInt(miners.size()));
+            writeMessage(Message.build(RobotType.FULFILLMENT_CENTER, miner.id));
+            addMessageToQueue();
+        }
+
 
 		if (builtMiners < 4) {
 			for (Direction dir : directions) {
