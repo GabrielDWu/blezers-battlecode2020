5ba103332d4fd906c672a7380b23989c8e841513
Kevin Higgs <github@kevinhiggs.com>
2020-01-25 12:16:03 -0500

    vaporator

---
+++ src/blezerbot/buildings/Vaporator.java
@@ -0,0 +1,13 @@
+package blezerbot.buildings;
+
+import battlecode.common.*;
+import java.util.*;
+import blezerbot.*;
+
+public class Vaporator extends Building {
+
+	public Vaporator(RobotController rc) throws GameActionException {
+		super(rc);
+	}
+
+}
