ee5b132419d0ef02df906b9af5347b0a363b8be7
galencolin <galencolin@gmail.com>
2020-01-25 20:55:33 -0500

    comment out prints?

--- src/blezerbot/units/Landscaper.java
+++ src/blezerbot/units/Landscaper.java
@@ -208,7 +208,7 @@ public class Landscaper extends Unit {
 								}
 							}
 
-							System.out.println(moveTries + " " + tryingClockwise);
+							// System.out.println(moveTries + " " + tryingClockwise);
 
 							if (!done) {
 								// System.out.println(isOurRobot(mloc.add(moveDir)) + " " + Math.abs(rc.senseElevation(mloc.add(moveDir)) - rc.senseElevation(mloc)));
